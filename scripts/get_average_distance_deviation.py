c30feaea3aedc4734901bf7f800fd5795dc96b45
Markus Schmidt <markus.rainer.schmidt@gmail.com>
2023-10-12 16:16:01 +0200

    gap fixing works!

--- scripts/get_average_distance_deviation.py
+++ scripts/get_average_distance_deviation.py
@@ -29,9 +29,10 @@ def get_average_distance_deviation(reads_in, mates_in, expected_distances_file):
             pos = int(pos)
             pos -= clipped
             flag = int(flag)
+            rev_stnd = flag & 16
             #print(qname, flag, rname, pos, sep='\t', file=sys.stderr)
             assert not qname in stored_reads
-            stored_reads[qname] = [rname, pos]
+            stored_reads[qname] = [rname, pos, rev_stnd]
 
     print("#columns:read_name distance_deviation")
     with fileinput.input(mates_in) as in_file3:
@@ -41,13 +42,18 @@ def get_average_distance_deviation(reads_in, mates_in, expected_distances_file):
             pos = int(pos)
             pos -= clipped
             flag = int(flag)
+            rev_stnd = flag & 16
             if qname in stored_reads and qname in expected_distances:
-                o_rname, o_rpos = stored_reads[qname]
+                o_rname, o_rpos, o_rev_stnd = stored_reads[qname]
 
                 if o_rname != rname:
                     continue
+                if rev_stnd != o_rev_stnd:
+                    continue
 
                 dist = pos - o_rpos
+                if rev_stnd:
+                    dist = o_rpos - pos
                 print(qname, dist - expected_distances[qname], sep='\t')
 
 
